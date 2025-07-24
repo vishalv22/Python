2d48f222a5d0694e141710f2867708883105f305
vishalv22 <vishalkumar.per@gmail.com>
2025-07-25 01:33:58 +0530

    finished 1_starting.py.py

--- Python/chapter 1/1_starting.py
+++ Python/chapter 1/1_starting.py
@@ -1,1 +1,24 @@
 print("Hello World!")
+
+a=2
+print(a)
+
+pi = 3.14
+print(pi)
+
+name = "Vishal"
+print(name)
+
+x = True
+print(x)
+
+z = None
+print(z)
+
+# Assigning multiple variables in one line
+a, b, c = 2, 3.14, "Vishal"
+print(a, b, c)
+
+# We can not use python's keywords as a valid name. We can see the list of keywords by:
+import keyword
+print(keyword.kwlist)
