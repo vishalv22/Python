e0deaee0832f50b019358beac30e0847af0204d2
vishalv22 <vishalkumar.per@gmail.com>
2025-07-25 01:22:47 +0530

    finished exercise_1.py

--- Python/chapter 1/2_modules.py
+++ Python/chapter 1/2_modules.py
@@ -1,5 +1,5 @@
 #Modules allow us to avoid writing everything from scratch by reusing existing, reliable code.
-#first install module using terminal, 'pip install pyjokes', and then import.
+#first install module using terminal, 'pip install pyjokes', and then import it.
 
 import pyjokes
 joke = pyjokes.get_joke()
--- Python/chapter 1/exercise_1.py
+++ Python/chapter 1/exercise_1.py
@@ -1,23 +1,23 @@
-#Q. print a poem using python.
+# 1. Python program to print the poem "Twinkle, Twinkle Little Star"
 
 print('''Twinkle, Twinkle Little Star
-How i wonder what you are?
+How I wonder what you are?
 Up above the world so high
 Like a diamond in the sky.''')
 
 
-#Q. Install the external module and use it to perform an operation of your interest.
-#pip install pyttx3, (text to speech module)
+# 2. Install the external module and use it to perform an operation of your interest.
+#pip install pyttsx3, (text to speech module)
 import pyttsx3
 engine = pyttsx3.init()
 engine.say("I will speak this text")
 engine.runAndWait()
 
 
-#Q. Write the python program to print the contents of a directory using the os module. Search online for the function which does that.
+# 3. Write the python program to print the contents of a directory using the os module. Search online for the function which does that.
 
 import os
-directory_path = "C:\\Users\\sonam\\Desktop"    # Specify the directory path
+directory_path = "C:\\Users\\sonam\\Desktop\\Books"    # Specify the directory path
 contents = os.listdir(directory_path)     # Get the list of contents (files and directories)
 print("Contents of", directory_path)     # Print the contents
 for item in contents:
