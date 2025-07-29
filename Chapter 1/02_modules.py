b634057e16a9dc487872d13ad26bff9b1c0a84a4
vishalv22 <vishalkumar.per@gmail.com>
2025-07-29 13:19:23 +0530

    docs(Chapter 1): add 02_modules.py

---
+++ Chapter 1/02_modules.py
@@ -0,0 +1,11 @@
+import pyjokes
+joke = pyjokes.get_joke()
+print(joke)               #this will print a random joke.
+
+
+# Built in module
+import math
+print(math.sqrt(16))     # 4.0
+
+dir (math) #this will show all the functions available in math module.
+#for example, we can use 'math.pi' to get the value of pi.
