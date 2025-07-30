32651326cbdc86e9ad778ca08c2edc9c483cb6a1
vishalv22 <vishalkumar.per@gmail.com>
2025-07-30 14:32:39 +0530

    docs(Chapter 2): add 03_variables.py

---
+++ Chapter 2/03_variables.py
@@ -0,0 +1,19 @@
+#VARIABLES
+
+a = 3
+b = 9 
+print(a + b)
+
+name = "Vishal"
+print(name)
+
+
+# Assigning multiple variables in one line
+a, b, c = 2, 3.14, "Vishal"
+print(a, b, c)
+
+
+
+# We can not use python's keywords as a valid name. We can see the list of keywords by:
+import keyword
+print(keyword.kwlist)
