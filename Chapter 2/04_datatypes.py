d61efb1d58691b491a11fea09f8132f23890500a
vishalv22 <vishalkumar.per@gmail.com>
2025-07-30 15:09:44 +0530

    docs(Chapter 2): add 04_datatypes.py

---
+++ Chapter 2/04_datatypes.py
@@ -0,0 +1,39 @@
+# DATATYPES
+
+# Integer
+num = 10
+print(num)
+print(type(num))    # Prints the type of Data 
+
+#same thing we can do like 
+print("Integer: ", num, "|", type(num))    #it will print value and type in same line
+
+
+
+# Float
+pi = 3.14
+print("Float:", pi, "|", type(pi))
+
+# String
+name = "Vishal"
+print("String:", name, "|", type(name))
+
+# Boolean
+i_m_vishal = True
+print("Boolean:", i_m_vishal, "|", type(i_m_vishal))
+
+# List
+fruits = ["apple", "banana", "guava"]
+print("List:", fruits, "|", type(fruits))
+
+# Tuple
+colors = ("black", "white", "orange")
+print("Tuple:", colors, "|", type(colors))
+
+# Set
+unique_numbers = {1, 2, 3}
+print("Set:", unique_numbers, "|", type(unique_numbers))
+
+# Dictionary
+person = {"name": "Vishal", "age": 18}
+print("Dictionary:", person, "|", type(person))
