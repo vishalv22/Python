d94748d957fa395b782d264397da9da52edac099
vishalv22 <vishalkumar.per@gmail.com>
2025-08-26 22:36:04 +0530

    docs(readme.md Ch5): add 10_list.py

---
+++ Chapter 5/10_list.py
@@ -0,0 +1,13 @@
+# A list contain items separated by commad and enclosed within square brackets []
+# List are almost similar to array in C.  one difference is that all the items belonging to a list can be of different data type.
+
+name = ["Apple", "Orange", 5, 25.6, False, "Vishal", "Ironman"]
+print(name[0])   # Apple 
+print(name[1])   # Orange 
+
+
+# if we do this, it will replace Orange → Grapes
+name[1] = "Grapes"
+print(name[1])  # Grapes
+
+
